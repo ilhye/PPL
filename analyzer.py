23d647b7be44014fd88b05924e04b9d760c0ff0c
ilhye <144883820+ilhye@users.noreply.github.com>
2025-01-29 14:20:31 +0800

    Enhance tokenizer to recognize comments, numbers, booleans, strings, and identifiers

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,6 @@
 import json
+import re
+
 
 class token:
     def __init__(self, token, lexeme):
@@ -8,26 +10,45 @@ class token:
     def __str__(self):
         return f"Token: {self.token}, Lexeme: {self.lexeme}"
 
+
 class analyzer:
     def __init__(self, contents):
         self.contents = contents
-    
+        # self.comments = []
+
     def tokenize(self):
         with open('tokens.json') as file:
             lexerData = json.load(file)
-        
-        no_whitespace = self.contents.replace(' ', ',').replace("\t", ",").replace("\n", ",").replace("\r", "")
-        no_duplicate = list(set(no_whitespace.split(",")))
-        split_contents = [part for part in no_duplicate if part]
 
+        no_whitespace = self.contents.replace(' ', ',').replace(
+            "\t", ",").replace("\n", ",").replace("\r", "")
+        no_duplicate = list(set(no_whitespace.split(",")))
         results = []
-        # Iterate over the lexerData dictionary and print the token and lexeme for the given lexeme
-        for part in split_contents:
-            for _,value in lexerData.items():
+
+        # Iterate ovtoker the lexerData dictionary and print the token and lexeme for the given lexeme
+        # Check if the lexeme is a number, float, boolean, string, comment, or identifier
+        for part in no_duplicate:
+            if re.match(r'/\*.*\*/', part):
+                results.append(token('COMMENT_CONTENT', part))
+            elif part.isdigit():
+                results.append(token('INT', part))
+            elif re.match(r'^\d+\.\d+$', part):
+                results.append(token('FLOAT', part))
+            elif part == "True" or part == "False":
+                results.append(token('BOOL', part))
+            elif re.match(r"\"[^\"]*\"", part):
+                results.append(token('STRING', part))
+            elif part.isidentifier() and part not in ["input", "output", r'/\*([a-zA-Z]+)\*/']:
+                results.append(token('IDENTIFIER', part))
+
+            # Check if the lexeme is a keyword
+            for _, value in lexerData.items():
                 for i in value:
                     if part == i['lexeme']:
                         results.append(token(i['token'], i['lexeme']))
+
         return results
+
 
 def parse(file):
     if not file.endswith('.smple'):
